525398b926deed77ada4d759352c5bae3fe42247
OttoElomaa <otto.elomaa99@gmail.com>
2024-10-26 23:39:04 +0300

    New viewcard.html Page. With Bootstrap table styles

--- src/main/java/hh/backend/mtgproject/web/CardController.java
+++ src/main/java/hh/backend/mtgproject/web/CardController.java
@@ -3,6 +3,7 @@ package hh.backend.mtgproject.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hh.backend.mtgproject.domain.CardRepository;
@@ -22,6 +23,13 @@ public class CardController {
     public String bookList(Model model) {
         model.addAttribute("cards", repository.findAll());
         return "cardlist";
+    }
+
+
+    @RequestMapping(value = "/viewcard/{id}")
+    public String viewCard(@PathVariable("id") Long cardId, Model model) {
+        model.addAttribute("selectedCard", repository.findById(cardId).get());
+        return "viewcard";
     }
 
 }
