8578d8cc37b5d3f9597cabff87ee1dc742f993a6
OttoElomaa <otto.elomaa99@gmail.com>
2024-10-27 12:42:33 +0200

    Added Index Page + IndexController. Added Page Links to webpage headers. Added "Back to list" button to viewcard.html

---
+++ src/main/java/hh/backend/mtgproject/web/IndexController.java
@@ -0,0 +1,16 @@
+package hh.backend.mtgproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+	@GetMapping(value = { "/", "/index" })
+	public String indexMethod(Model model) {
+
+		return "index";
+	}
+}
