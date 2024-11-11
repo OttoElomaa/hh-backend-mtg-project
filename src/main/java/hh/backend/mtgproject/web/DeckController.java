a3d5a740499f20050cfc6f641c0a87222fc251ab
OttoElomaa <otto.elomaa99@gmail.com>
2024-11-11 14:30:47 +0200

    Added Delete Deck button in myprofile. With some security stuff via PreAuthorize and Principal check

--- src/main/java/hh/backend/mtgproject/web/DeckController.java
+++ src/main/java/hh/backend/mtgproject/web/DeckController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -111,6 +112,25 @@ public class DeckController {
 
 		return "redirect:/viewdeck/" + deck.getDeckId().toString();
 	}
+
+
+
+	// DELETE FUNCTION FOR DECKS SELECTED IN MYPROFILE.HTML
+
+	@PreAuthorize("hasRole('USER')")
+    @GetMapping(value = "/deletedeck/{id}")
+    public String deleteBook(@PathVariable("id") Long deckId, Model model, Principal principal) {
+
+		setUserIfLogged(principal, model, userRepository);
+		Deck selectedDeck = repository.findById(deckId).get();
+		MtgUser currUser = userRepository.getByUserName(principal.getName());
+
+		if (currUser == selectedDeck.getUser()) {
+        	repository.deleteById(deckId);
+        	return "redirect:/myprofile";
+		}
+		return "redirect:/myprofile";
+    }
 
 
 
